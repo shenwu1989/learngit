f1177ce2fee235601a89c7f7795a8c803929cf8d
shenwu <shenwu1989@mifengkong.cn>
2019-03-20 18:55:11 +0800

    fix

--- src/App.js
+++ src/App.js
@@ -1,28 +1,33 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
 import './App.css';
+import styled from 'styled-components';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <H1>中华</H1>
+        <H2 size={true}>人民</H2>
+       
       </div>
     );
   }
 }
 
+
 export default App;
+
+const H1 = styled.h1`
+  color:red;
+  background-color:yellow;
+  width:100%;
+  height:calc(30vh);
+  text-align:center;
+border:1px solid #000;
+`
+const H2 = styled(H1)`
+  color:green;
+  font-size:${props => props.size ? '130px' : '15px'}
+`
